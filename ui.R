f37aadc0070e8f40f65e2154d8dc86f6ad31fced
Kevin Wang <kevin.wang@sydney.edu.au>
2020-03-13 22:59:19 -0400

    Prettifying the app and add instructions

--- ui.R
+++ ui.R
@@ -1,15 +1,15 @@
 shinyUI(fluidPage(
-    titlePanel("Kevin's example nCov app"),
-    sidebarLayout(
-        sidebarPanel(
+    titlePanel("COVID-19: visualising added cases in selected countries"),
+    # sidebarLayout(
+        # sidebarPanel(
             selectInput(inputId = "country",
                           label = "Select some countries",
                           choices = all_data['global']$country %>% unique,
-                          multiple = FALSE)
-        ),
-        mainPanel(
+                          multiple = FALSE),
+        # ),
+        # mainPanel(
             shiny::plotOutput(outputId = "cum_confirm_plot"),
             shiny::plotOutput(outputId = "added_plots", height = "800px")
-        )
-    )
+        # )
+    # )
 ))
